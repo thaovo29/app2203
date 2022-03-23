44b8fd0eefedfe874be8b443bc81d2b5809ef32f
omachi-kokomi <thaovo2962002@gmail.com>
2022-03-23 17:33:49 +0700

    commit

---
+++ screens/tracnghiem/xahoihoa.js
@@ -0,0 +1,124 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React, { useState } from 'react';
+ import { Image, Button, Alert} from 'react-native';
+ import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ const Section = ({children, title}): Node => {
+   const isDarkMode = useColorScheme() === 'dark';
+   return (
+     <View style={styles.sectionContainer}>
+       <Text
+         style={[
+           styles.sectionTitle,
+           {
+             color: isDarkMode ? Colors.white : Colors.black,
+           },
+         ]}>
+         {title}
+       </Text>
+       <Text
+         style={[
+           styles.sectionDescription,
+           {
+             color: isDarkMode ? Colors.light : Colors.dark,
+           },
+         ]}>
+         {children}
+       </Text>
+     </View>
+   );
+ };
+ 
+ const xahoihoa  = () => {
+   const isDarkMode = useColorScheme() === 'dark';
+   const [isPress,setState] = useState(true)
+   var a = 0;
+   const add = () =>{
+     a += 1;
+     console.log(a);
+   }
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   };
+ 
+   return (
+     <SafeAreaView style={backgroundStyle}>
+       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+       <ScrollView
+         contentInsetAdjustmentBehavior="a)utomatic"
+         style={backgroundStyle}>
+         <Header />
+         <View
+           style={{
+             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+           }}>
+             <Text> Hello world</Text>
+             {/* //<View style = {styles.img}> */}
+               <Image style = {styles.img} source = {require("../image/tracnghiem/xahoihoa.png")}/>
+             {/* </View> */}
+         </View>
+         <View style ={{margin: 20}}>
+           <Button
+             title = "click me"
+             color = "#841584"
+             onPress={add}
+           />
+         </View>
+       </ScrollView>
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+   img: {
+     height: 200,
+     width: 200,
+     justifyContent: 'center',
+     alignContent: 'center',
+     marginLeft: 80,
+     marginTop: 20
+   }
+ });
+ 
+ export default xahoihoa;
+ 
